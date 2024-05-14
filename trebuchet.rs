13edff59b97946e8a043d5daf7b3939346732a6a
Hargun Singh Tikku <hargunsinghtikku@gmail.com>
2024-05-14 12:34:43 +0530

    Incomplete part 2

--- trebuchet.rs
+++ trebuchet.rs
@@ -1,15 +1,24 @@
 use std::fs::read_to_string;
-use std::fs::File;
 
 fn main() {
-    println!("{}", compute("trebuchet-data"));
+    println!("{}", compute("trebuchet-data", true));
 }
 
-fn compute(filename: &str) -> u32 {
+fn compute(filename: &str, part_2 : bool) -> u32 {
     let mut counter: u32 = 0;
 
     for line in read_to_string(filename).unwrap().lines() {
+      if !part_2{
         counter += string_to_num(line);
+      }
+      else {
+        let string = line.replace("one", "1").replace("two", "2")
+          .replace("three", "3").replace("four", "4").replace("five", "5")
+          .replace("six", "6").replace("seven", "7").replace("eight", "8")
+          .replace("nine", "9");
+        println!("{}", string);
+        counter += string_to_num(&string);
+      }
     }
 
     counter
@@ -19,7 +28,7 @@ fn string_to_num(string: &str) -> u32 {
     let mut first_int: u32 = 0;
     let mut last_int: u32 = 0;
 
-    for (index, char) in string.char_indices() {
+    for (_index, char) in string.char_indices() {
         if char >= '1' && char <= '9' {
             last_int = char.to_digit(10).unwrap();
             if first_int == 0 {
